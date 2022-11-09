83044fda29d3710987010a0e3fa2b62fed2390e7
Basil Goryachev <lagom@ecwid.com>
2022-11-09 13:34:22 +0400

    Updates

---
+++ assets/transport.db85710e.js
@@ -0,0 +1,1 @@
+import{g as u,h as e}from"./index.2f1a8dac.js";import{A as n}from"./arriving.dfdb1a8b.js";const f=u("transport",()=>{const o=e([]);n.getAllRoutes().then(t=>o.value=t);const r=e([]);n.getAllStops().then(t=>r.value=t.sort((l,p)=>parseInt(l.code)>parseInt(p.code)?1:-1));const s=e(0);function a(){s.value++}return{allRoutes:o,allStops:r,increment:a}});export{f as u};

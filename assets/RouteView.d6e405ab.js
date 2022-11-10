b215fa18d20c4e9684f51c1c355ce850e8159e9d
Basil Goryachev <lagom@ecwid.com>
2022-11-10 10:43:28 +0400

    Updates

---
+++ assets/RouteView.d6e405ab.js
@@ -0,0 +1,1 @@
+import{d as e,t as o,u as r,i as s}from"./index.408f697c.js";const m=e({__name:"RouteView",setup(n){const t=s().params.routeId.toString();return(a,i)=>"This is Route page. Not implemented. "+o(r(t))}});export{m as default};

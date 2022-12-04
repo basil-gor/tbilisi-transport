3cd1a14d36dc4df88305949cd9ea3c6891cac7f9
Basil Goryachev <lagom@ecwid.com>
2022-12-04 12:47:04 +0400

    Updates

--- assets/RoutesView.5565377d.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./transport.275ce726.js";import{d as s,s as r,n as t,u as a,o as n}from"./index.5d01a30b.js";import{_ as u}from"./RoutesTable.vue_vue_type_script_setup_true_lang.52a9dbee.js";const l=s({__name:"RoutesView",setup(_){const{allRoutes:o}=r(e());return(c,m)=>(n(),t(u,{routes:a(o)},null,8,["routes"]))}});export{l as default};
---
+++ assets/StopsView.68816289.js
@@ -0,0 +1,1 @@
+import{u as s}from"./transport-stops.e153c93d.js";import{d as t,s as r,u as e,x as a,h as p,o as n}from"./index.d54b0622.js";import{S as m}from"./StopsTable.94bbcf25.js";import"./FavoriteButton.434ebb35.js";const d=t({__name:"StopsView",setup(c){const{allStops:o}=r(s());return(u,l)=>e(o).length!==0?(n(),a(m,{key:0,stops:e(o)},null,8,["stops"])):p("",!0)}});export{d as default};
